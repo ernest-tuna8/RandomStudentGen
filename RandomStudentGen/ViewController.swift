c41fe6d0746d0252884a3d6b174209f0bed168e1
clc babischkin <n/a>
2021-03-18 08:02:22 -0500

    final commit

--- RandomStudentGen/ViewController.swift
+++ RandomStudentGen/ViewController.swift
@@ -21,21 +21,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var LowerImage: UIImageView!
     
     @IBOutlet weak var MainTextField: UITextField!
-    @IBOutlet weak var LabelOutlet1: UILabel!
-    @IBOutlet weak var LabelOutlet2: UILabel!
+   
     @IBOutlet weak var LabelOutlet3: UILabel!
-    @IBOutlet weak var LabelOutlet4: UILabel!
-    @IBOutlet weak var LabelOutlet5: UILabel!
-    @IBOutlet weak var LabelOutlet6: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        LabelOutlet1.isHidden = true
-        LabelOutlet2.isHidden = true
+       
         LabelOutlet3.isHidden = true
-        LabelOutlet4.isHidden = true
-        LabelOutlet5.isHidden = true
-        LabelOutlet6.isHidden = true
         UpperImage.image = UIImage(named: "thecrew")
         LowerImage.image = UIImage(named: "Choose")
     }
@@ -49,64 +41,12 @@ class ViewController: UIViewController {
     
     @IBAction func GenerateButton(_ sender: UIButton) {
         //randomly picking students, filing into labels
-        var count2 = 0
-        var count3 = 0
-        var count4 = 0
-        var count5 = 0
-        var count6 = 0
-        var count7 = 0
+        
         let alert = UIAlertController(title: "No Students", message: "There are no students entered", preferredStyle: .alert)
         let ExitAlert = UIAlertAction(title: "Ok", style: .default, handler: nil)
         alert.addAction(ExitAlert)
-        for finder in students {
-            let randomgroup = Int.random(in: 1...groups)
-            if randomgroup == 1 {
-                group1[count2] = finder.value
-                count2 = count2 + 1
-            }
-            else if randomgroup == 2 {
-                group2[count3] = finder.value
-                count3 = count3 + 1
-            }
-            else if randomgroup == 3 {
-                group3[count4] = finder.value
-                count4 = count4 + 1
-            }
-            else if randomgroup == 4 {
-                group4[count5] = finder.value
-                count5 = count5 + 1
-            }
-            else if randomgroup == 5 {
-                group5[count6] = finder.value
-                count6 = count6 + 1
-            }
-            else {
-                group6[count7] = finder.value
-                count7 = count6 + 1
-            }
-            LabelOutlet1.isHidden = false
-            LabelOutlet1.text = "\(group1)"
-            LabelOutlet2.isHidden = false
-            LabelOutlet2.text = "\(group2)"
-            if group3 != nil {
-                LabelOutlet3.isHidden = false
-                LabelOutlet3.text = "\(group3)"
-            }
-            if group4 != nil {
-                LabelOutlet4.isHidden = false
-                LabelOutlet4.text = "\(group4)"
-            }
-            if group5 != nil {
-                LabelOutlet5.isHidden = false
-                LabelOutlet5.text = "\(group5)"
-            }
-            if group6 != nil {
-                LabelOutlet6.isHidden = false
-                LabelOutlet6.text = "\(group6)"
-            }
-            
-        }
-        
+            let randomstudent = Int.random(in: 1...students.capacity)
+        LabelOutlet3.text = "\(students[randomstudent])"
     }
     
     @IBAction func GroupAmount(_ sender: UISegmentedControl) {
